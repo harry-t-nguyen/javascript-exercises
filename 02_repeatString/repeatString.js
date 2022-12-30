c0b18d265334410630985858ff3d11e9170f550e
harry-t-nguyeen <harrytnguyeen@gmail.com>
2022-12-30 21:47:19 +1100

    finish 01,02,03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
+const repeatString = function(word,x) {
+    let result ='';
+    if (x<0){
+        return result = "ERROR"
+    } else {
+        for (i=0; i<x; i++){
+            result+=word
+        }
+        return result
+    }
 };
+
+// let result = repeatString('hello',10);
+// console.log(result);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseword = '';
+    let splitword = word.split('');
+    for (i=splitword.length - 1; i>=0; i--){
+        reverseword+= splitword[i];
+    }
+    return reverseword
 };
 
+console.log(reverseString('hello world'));
+
+// let test = '';
+// let word = "abc de";
+// let substring = word.split('');
+
+// test += substring[5];
+// console.log(test);
+// console.log(substring[5]);
+
+// console.log(substring.length);
 // Do not edit below this line
 module.exports = reverseString;
