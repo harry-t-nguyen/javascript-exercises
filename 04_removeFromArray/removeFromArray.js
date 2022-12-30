495f574085de6dab9e9fba6a32d649d5062ab8c0
harry-t-nguyen <harrytnguyeen@gmail.com>
2022-12-31 02:25:17 +1100

    complete excercise 4 & 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeitems) {
+    let position;
+    //Loop through the items that needs to be removes
 
-};
+    for (i=0; i<removeitems.length; i++){
+        //find the position of the remove items
+        position = array.indexOf(removeitems[i])
 
+        //remove only when items found in the array
+        if (position != -1){
+            array.splice(position,1)
+        }
+        }
+    return array;
+    }
+
+console.log(removeFromArray([1,2,3,4,5,6], 5, 6))
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (typeof a != 'number' || typeof b !=  'number'){
+        return "ERROR"
+    } else if ( a < 0 || b < 0){
+        return "ERROR"
+    } else {
+        if (a < b){
+            for (i=a; i<=b; i++){
+            sum += i;
+            }
+        } else {
+        for (x=b; x<=a; x++){
+            sum += x;
+        } 
+    }
+    return sum;
+    }
 
-};
+        // console.log(typeof a)
+        // console.log(typeof b)
+        // console.log(Number.isInteger(a))
+        // console.log(Number.isInteger(b))
+}
 
-// Do not edit below this line
+
+// console.log(sumAll(1,4))
+
+//Do not edit below this line
 module.exports = sumAll;
